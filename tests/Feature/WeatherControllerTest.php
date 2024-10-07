0523acf6f351a769e1d80996598b7f310ca31d94
NgBlaze <okolaaprecious@gmail.com>
2024-10-07 21:12:58 +0100

    chore: Added addional Test cases

--- tests/Feature/WeatherControllerTest.php
+++ tests/Feature/WeatherControllerTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class WeatherControllerTest extends TestCase
 {
@@ -18,5 +19,33 @@ class WeatherControllerTest extends TestCase
         $response->assertStatus(201);
 
         $this->assertEquals('london', $location);
+    }
+
+    public function test_if_invalid_location_gives_appropriate_error_message()
+    {
+        $response = $this->getJson('api/v1/weather/londonnnnnn');
+        $response->assertStatus(400);
+
+        $response->assertJson([
+            "status_code" => 400,
+            "message" => "Invalid Location",
+        ]);
+    }
+
+    public function test_if_redis_caches_location_and_retrieves_location_correctly()
+    {
+        $response = $this->getJson('api/v1/weather/monaco');
+        $response->assertStatus(201);
+        $response->assertJson([
+            "status_code" => 201,
+            "message" => "Weather data retrieved successfully",
+        ]);
+
+        $cached_response = $this->getJson('api/v1/weather/monaco');
+        $cached_response->assertStatus(201);
+        $cached_response->assertJson([
+            "status_code" => 201,
+            "message" => "Weather data retrieved from cache successfully",
+        ]);
     }
 }
